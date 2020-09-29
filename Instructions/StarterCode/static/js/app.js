2cfa534dff8a69fb27397b14f6b302405793f0be
Hayden Rubin <haydenrubin@gmail.com>
2020-09-29 10:22:49 -0700

    commit

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -99,9 +99,10 @@ init();
 
 //
 
+
 // Step 5: Display each key-value pair from the metadata JSON object somewhere on the page. 
 
-
+//
 
 // Step 6: Update all of the plots any time that a new sample is selected (Day 2 Activity 8).
 
